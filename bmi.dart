f588bdaf824185d7ed38ae8b39d8122a7ac5c058
Tommogo-coder <nelsontommogo9@gmail.com>
2023-06-02 15:56:05 +0300

    Dart

---
+++ bmi.dart
@@ -0,0 +1,19 @@
+void main() {
+  double weight = 56;
+  double height = 1.75;
+  double bmi = weight / (height * height);
+
+  String status = "";
+
+  if (bmi < 15.5) {
+    status = "Underweight";
+  } else if (bmi >= 15.5 && bmi <= 18.9) {
+    status = "Healthy Weight";
+  } else if (bmi >= 18.9 && bmi <= 30.9) {
+    status = "Overweight";
+  } else {
+    status = "Obesity";
+  }
+
+  print("BMI $bmi: Status: $status");
+}
