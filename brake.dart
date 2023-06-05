83ef0854df2e74e01c2d8ed643055be0a79b32ea
Tommogo-coder <nelsontommogo9@gmail.com>
2023-06-05 20:30:31 +0300

    Dart Sessions

---
+++ brake.dart
@@ -0,0 +1,15 @@
+void main( )
+{
+    int trial = 0 ;
+  
+    while ( trial <= 15 ) 
+    {
+        print( trial ) ;
+        trial ++ ;
+  
+        if ( trial == 15 ) 
+        {
+            break ;
+        }
+    }
+}
