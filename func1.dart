e4c44d50d0f0930b64c3db8240d3dcb4a8a942a8
Tommogo-coder <nelsontommogo9@gmail.com>
2023-06-05 20:38:08 +0300

    Dart Sessions

---
+++ func1.dart
@@ -0,0 +1,66 @@
+import 'dart:io' ;
+
+void main( )
+
+{
+
+  int i, flag = 0 ;
+
+  print( ' Enter the number : ' ) ;
+
+  int? n = int.parse( stdin.readLineSync( ) ! ) ;
+
+
+
+
+  flag = prime( n ) ;
+
+  if (n == 1) {
+
+    print( " 1 is neither prime nor composite. " ) ;
+
+  } else {
+
+    if (flag == 0)
+
+      print( " Number is a prime number. " ) ;
+
+    else
+
+      print( " Number is not a prime number. " ) ;
+
+  }
+
+}
+
+
+
+
+int prime( int n )
+
+{
+
+  int flag = 0 ;
+
+  for ( int i = 2; i <= n / 2; i++ )
+
+  {
+
+    if ( n % i == 0 )
+
+   {
+
+      flag = 1 ;
+
+      break ;
+
+    }
+
+  }
+
+
+
+
+  return flag ;
+
+}
