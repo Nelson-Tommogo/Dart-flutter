67e5331fdd02be7d4dde408e42f0de58f2613af2
Tommogo-coder <nelsontommogo9@gmail.com>
2023-06-07 10:41:47 +0300

    Dart

--- dartgen.dart
+++ dartgen.dart
@@ -1,14 +1,16 @@
 // Program of Dart Asynchronous Generator 
 void main( )  
 {  
-    print( " List of first 20 whole numbers are as follows : " ) ;  
+    print( " List of first  whole numbers are as follows : " ) ;  
     asyncNaturalsTo( 20 ).forEach( print ) ;   
 }    
+// async* functions returns a stream object  
 Stream< int > asyncNaturalsTo( int num ) async* 
 {  
     int temp = 0 ;  
     while( temp < num ) 
     {  
+        // ' yield ' statement  
         yield temp++ ;   
     }  
   temp-- ;  
