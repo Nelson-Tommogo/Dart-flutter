fa29dade9fa280c9ac212c33211b15338185de92
Tommogo-coder <nelsontommogo9@gmail.com>
2023-06-07 10:43:59 +0300

    Dart

--- temp.dart
+++ temp.dart
@@ -3,7 +3,6 @@ void main( )
     print( " This is the list of even numbers between 1 to 15  : " ) ;  
     oddNumber( 15 ).forEach( print ) ;   
 } 
-// sync* functions returns an iterable  
 Iterable< int > oddNumber( int n ) sync* 
 {  
     int temp = n ;  
