12b3fb1c7f1ea9232956198046b78c72775c6b33
Tommogo-coder <nelsontommogo9@gmail.com>
2023-06-26 14:18:12 +0300

    Dart

--- fun.dart
+++ fun.dart
@@ -4,11 +4,13 @@ void main() {
   List<int> subjectScores = getSubjectScores();
   double average = calculateAverage(subjectScores);
   print('Your Average: $average');
+  double sumsubj = calculateAverage(subjectScores);
+  print('Sum of the subjects= $sumsubj');
 }
 
 List<int> getSubjectScores() {
   List<int> subjectScores = [];
-  for (int i = 1; i <= 6; i++) {
+  for (int i = 1; i <= 12; i++) {
     print('Enter the score for subject $i:');
     int score = int.parse(stdin.readLineSync()!);
     subjectScores.add(score);
@@ -23,4 +25,13 @@ double calculateAverage(List<int> scores) {
   }
   double average = sum / scores.length;
   return average;
+}
+
+double sumsubj(List<int> scores) {
+   int sumsubj = 0;
+  for (int score in scores) {
+    sumsubj += score;
+  }
+  double sumsubj = sumsubj / scores.length;
+  return sumsubj;
 }
