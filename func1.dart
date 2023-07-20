82a3954731a8ec8e7908491302781e932c534d64
Tommogo-coder <nelsontommogo9@gmail.com>
2023-07-20 12:37:30 +0300

    Dart

--- func1.dart
+++ func1.dart
@@ -1,4 +1,3 @@
-
 void calculate() {
   int num1 = 30;
   int num2 = 33;
@@ -7,7 +6,26 @@ void calculate() {
   print("sum is $sum");
 }
 
+void grading() {
+  int unit1 = 66;
+  int unit2 = 91;
+  int unit3 = 79;
+  int unit4 = 87;
+  int unit5 = 39;
+  int unit6 = 50;
+  int unit7 = 29;
+  int unit8 = 95;
+  int unit9 = 71;
+
+  int total =
+      unit9 + unit8 + unit7 + unit6 + unit5 + unit4 + unit3 + unit2 + unit1;
+  // int avarage = (total / 9);
+  // int gpa = (avarage / 10);
+  print("total marks is $total");
+}
+
 void main() {
   print("Something Great today");
   calculate();
+  grading();
 }
