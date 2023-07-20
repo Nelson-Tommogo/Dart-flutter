45f7c740f6dc4c8d1f01ef9816042f1ce65fae47
Tommogo-coder <nelsontommogo9@gmail.com>
2023-07-20 12:28:52 +0300

    Dart

--- func1.dart
+++ func1.dart
@@ -1,34 +1,13 @@
-import 'dart:io';
 
-void main() {
-  int i, flag = 0;
-
-  print(' Enter any  number : ');
-
-  int? n = int.parse(stdin.readLineSync()!);
-
-  flag = prime(n);
-
-  if (n == 1) {
-    print(" 1 is neither prime nor composite. ");
-  } else {
-    if (flag == 0)
-      print(" prime number. ");
-    else
-      print(" not a prime number. ");
-  }
+void calculate() {
+  int num1 = 30;
+  int num2 = 33;
+  int num3 = 5;
+  int sum = num1 + num2 + num3;
+  print("sum is $sum");
 }
 
-int prime(int n) {
-  int flag = 0;
-
-  for (int i = 2; i <= n / 2; i++) {
-    if (n % i == 0) {
-      flag = 1;
-
-      break;
-    }
-  }
-
-  return flag;
+void main() {
+  print("Something Great today");
+  calculate();
 }
